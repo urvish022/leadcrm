ef75ac51566d1a51449547022c93df69d0ee67ad
urvish <urvish31797@gmail.com>
2022-12-16 13:15:46 +0530

    added csrf

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,6 +3,7 @@
 <head>
     <base href="./">
     <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Login | {{ config('app.name') }}</title>
